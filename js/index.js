8f47138c6c69b58b8ec178a9aa0a4887c05520a4
Ignacio Sanchez <sanchezcreativo@gmail.com>
2024-06-07 16:05:59 -0400

    Minor improvements player

--- js/index.js
+++ js/index.js
@@ -66,6 +66,7 @@ document.addEventListener('DOMContentLoaded', function () {
     }, 3000);
 
     function playAudio() {
+        var audio = document.getElementById('audio');
         audio.play().catch(error => {
             console.error('Error al reproducir el audio:', error);
         });
@@ -97,6 +98,7 @@ document.addEventListener('DOMContentLoaded', function () {
         // verificar si audio no está en play, darle play:
         if (audio.paused) {
             playAudio();
+            var audio = document.getElementById('audio');
             audio.setAttribute('title', document.getElementById('song').textContent);
             audio.setAttribute('poster', document.getElementById('cover').src);
         }
@@ -112,13 +114,116 @@ document.addEventListener('DOMContentLoaded', function () {
         logo.classList.add('active'); // Activar la animación del logo
         updateSongInfo(); // Forzar una actualización al pulsar el botón
     });
-
-
+    // crear funcion que obtenga y retorne la data para ser usada posteriormente:
+    function getRadioData() {
+        fetch('https://radio.laurban.cl/api/nowplaying/laurban')
+            .then(response => response.json())
+            .then(data => {
+                return data;
+            })
+            .catch(error => console.error('Error al obtener la información de la canción:', error));
+    }
 
     function updateSongInfo() {
         const defaultCover = 'https://laurban.cl/img/default.jpg';
         setThemeByTime();
-        fetch('https://radio.laurban.cl/api/nowplaying/laurban')
+        var data = getRadioData();
+        changeMediaData(data);
+        if (data.live.is_live) {
+            dynamicButton.href = "whatsapp://send/?phone=+56949242000&abid=+56949242000&text=Escribe%20aca%20tu%20saludo%20y%20pedido%20musical.%20Tambien%20puedes%20enviar%20mensaje%20de%20voz";
+            buttonIcon.className = 'fab fa-whatsapp';
+            buttonText.textContent = 'Escríbenos';
+            dynamicButton.onclick = null; // Elimina cualquier otro evento onclick
+        } else {
+            dynamicButton.href = "#"; // Evita que el enlace redirija
+            buttonIcon.className = 'fas fa-music';
+            buttonText.textContent = 'Pedir canción';
+            dynamicButton.onclick = function (event) {
+                event.preventDefault(); // Previene la acción por defecto del enlace
+                musicRequestCanvas.classList.toggle('open');
+            };
+        }
+        var cover = document.getElementById('cover');
+        var song = document.getElementById('song');
+        var title = document.title;
+
+        // Verificar que data.now_playing y sus propiedades existen
+
+        if (
+            data.now_playing
+            && data.now_playing.song
+            && data.now_playing.song.artist
+            && data.now_playing.song.title
+        ) {
+            // Extraer la parte antes y después del punto y coma
+            var [mainArtist, ...extraArtists] = data.now_playing.song.artist.split(';').map(part => part.trim());
+
+            // Variables para el artista y el título corregidos
+            var artistName = mainArtist;
+            var songName = data.now_playing.song.title;
+
+            // Extraer los artistas extra y unirlos en un string
+            var extraArtistsText = extraArtists.join(', ');
+
+            // Verificar si el título contiene la parte extra del artista
+            if (songName.includes(extraArtistsText)) {
+                // Si el título contiene la parte extra, solo actualizamos el artist
+                artistName = mainArtist;
+            } else if (extraArtistsText) {
+                // Si no, actualizamos el artist y modificamos el título
+                artistName = mainArtist;
+                songName = `${songName} (feat. ${extraArtistsText})`;
+            } else {
+                // Si no hay parte extra, solo actualizamos el artist
+                artistName = mainArtist;
+            }
+
+            // Crear la variable songText que es el merge de ambas
+            var songText = `Escuchas: ${artistName} - ${songName}`;
+
+            // Actualizar la información de la canción
+            cover.src = data.now_playing.song.art ? data.now_playing.song.art : defaultCover;
+            song.textContent = songText;
+
+            // Actualizar el título de la página
+            title = `La Urban · Reproduciendo: ${artistName} - ${songName}`;
+
+            // Pasar información de la canción y la carátula al reproductor
+            audio.setAttribute('title', songText);
+            audio.setAttribute('poster', cover.src);
+        } else {
+            // Manejo de caso donde no se obtiene la información esperada
+            cover.src = defaultCover;
+            song.textContent = defaultTitle;
+            title = defaultTitle;
+        }
+
+        // hacer lo mismo con el playing_next que sería lo que viene y reemplazar el next-song:
+        var nextSong = document.getElementById('next-song');
+
+        if (data.live.is_live) {
+            nextSong.textContent = '¡Estamos en vivo!';
+        } else {
+            if (data.playing_next && data.playing_next.song && data.playing_next.song.artist && data.playing_next.song.title) {
+                var [mainArtist, extraArtist] = data.playing_next.song.artist.split(';').map(part => part.trim());
+                var artistName = mainArtist;
+                var songName = data.playing_next.song.title;
+                if (songName.includes(extraArtist)) {
+                    artistName = mainArtist;
+                } else if (extraArtist) {
+                    artistName = mainArtist;
+                    songName = `${songName} (feat. ${extraArtist})`;
+                } else {
+                    artistName = mainArtist;
+                }
+                var songText = `Ya viene: ${artistName} - ${songName}`;
+                nextSong.textContent = songText;
+            } else {
+                nextSong.textContent = defaultTitle;
+            }
+        }
+    }
+/*         fetch('https://radio.laurban.cl/api/nowplaying/laurban')
             .then(response => response.json())
             .then(data => {
                 changeMediaData(data);
@@ -215,7 +320,7 @@ document.addEventListener('DOMContentLoaded', function () {
                     }
                 }
             })
-            .catch(error => console.error('Error al obtener la información de la canción:', error));
+            .catch(error => console.error('Error al obtener la información de la canción:', error)); */
     }
 
     // Actualizar la información de la canción cada 30 segundos
